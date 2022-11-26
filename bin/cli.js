30e687d4a70bf8ed0fc1ffe3276a16606ba76fa4
Matthew Kolsch <kolsch@email.unc.edu>
2022-11-25 22:06:09 -0500

    hopefully functional code

--- bin/cli.js
+++ bin/cli.js
@@ -1,18 +1,26 @@
 #!/usr/bin/env node
 // File that runs (when the package is linked/installed) and roll-dice is ran
 
-import { roll } from "/lib/roll.js";
-import minimist from minimist;
+import { roll } from "../lib/roll.js";
+import minimist from 'minimist';
 
 const argument = minimist(process.argv.slice(2));
 
 // Can't have a negative num_sides, num_dice, or num_rolls
-for (arg in argument) {
-	if (arg < 0) {
-		console.log("Arguments cannot be negative.")
-		process.exit(1);
-	}
+// Why does "console.log(argument.arg);" not work here?
+// for (let arg in argument) {
+//	console.log(argument.arg);
+//	if (argument.arg < 0) {
+//		console.log("Arguments cannot be negative.");
+//		process.exit(1);
+//	}
+//}
 
-const roll_json = roll(argument.sides, argument.dice, argument.rolls);
+if (argument.sides < 0 || argument.dice < 0 || argument.rolls < 0) {
+	console.log("Arguments cannot be negative.");
+	process.exit(1);
+}
+
+const roll_json = roll(argument.sides || 6, argument.dice || 2, argument.rolls || 1);
 
 console.log(JSON.stringify(roll_json));
